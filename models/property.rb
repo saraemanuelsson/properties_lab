dd54adba5ade56dbe6ce11497181ca74a6ff4667
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-05-26 15:15:45 +0100

    Add save method

--- models/property.rb
+++ models/property.rb
@@ -1,11 +1,32 @@
+require('pg')
+
 class Property
 
     attr_accessor :address, :value, :square_footage, :build
+    attr_reader :id
 
     def initialize(options)
+        @id = options['id'].to_i if options['id']
         @address = options['address']
         @value = options['value']
         @square_footage = options['square_footage']
         @build = options['build']
+    end
+
+    def save
+        db = PG.connect( { dbname: 'properties', host: 'localhost' } )
+        sql = "INSERT INTO properties
+            (
+            address,
+            value,
+            square_footage,
+            build
+            )
+            VALUES ($1,$2,$3,$4)
+            RETURNING id"
+        values = [@address, @value, @square_footage, @build]
+        db.prepare("save", sql)
+        @id = db.exec_prepared("save", values)[0]["id"].to_i
+        db.close
     end
 end
